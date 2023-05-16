9f2af86d1347d595a4cd6ccdec9ae2900dea6330
Dragan Domazet <d.domazet@beeit.rs>
2023-05-16 17:38:09 +0200

    Started configuration

--- OnlineStore/OnlineStore.Api/Program.cs
+++ OnlineStore/OnlineStore.Api/Program.cs
@@ -2,7 +2,12 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OnlineStore.Database;
+using System.Text;
+
+string _cors = "cors";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +25,66 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Termin_6", Version = "v1" });
+    //Ovo dodajemo kako bi mogli da unesemo token u swagger prilikom testiranja
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+});
+
+builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+           .AddJwtBearer(options =>
+           {
+               options.TokenValidationParameters = new TokenValidationParameters //Podesavamo parametre za validaciju pristiglih tokena
+               {
+                   ValidateIssuer = true, //Validira izdavaoca tokena
+                   ValidateAudience = false, //Kazemo da ne validira primaoce tokena
+                   ValidateLifetime = true,//Validira trajanje tokena
+                   ValidateIssuerSigningKey = true, //validira potpis token, ovo je jako vazno!
+                   ValidIssuer = "http://localhost:44398", //odredjujemo koji server je validni izdavalac
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings")["SecretKey"]))//navodimo privatni kljuc kojim su potpisani nasi tokeni
+               };
+           });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: _cors, builder =>
+    {
+        builder.WithOrigins("https://localhost:4200")//Ovde navodimo koje sve aplikacije smeju kontaktirati nasu,u ovom slucaju nas Angular front
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
 
 var app = builder.Build();
 
@@ -32,6 +96,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(_cors);
+
+app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
